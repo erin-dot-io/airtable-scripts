529a5be94b76e797ad5a96f35fb2d25e94b33a93
Erin Keeffe <erin@erin.io>
2021-09-29 18:54:49 -0400

    update manual merge script, add import script

--- finance/manual-merge.js
+++ finance/manual-merge.js
@@ -1,3 +1,10 @@
+// INPUT VARIABLES
+// id = Airtable record ID
+// merchant_name = Field values / Merchant / Name
+// amount = Field values / **USD
+// import_name = Field values / *Name
+// import_tags = Field values / Tags / Name
+
 let config = input.config();
 let importId = config.id;
 let importMerchant = config.merchant_name;
@@ -7,43 +14,65 @@ let importTags = config.import_tags;
 
 let transactions = base.getTable('Transactions');
 let transactionsFiltered = await transactions.getView('Manual Add Check').selectRecordsAsync({
-        fields: ['Merchant', 'Transaction Date', '**USD', 'Attachments', 'Personal Note', 'Tags']
-    });
-let thisTransaction = transactionsFiltered.getRecord(importId);
-
-let manualAdds = await base.getTable('Transactions').getView('Manual Add').selectRecordsAsync({
-    fields: ['Merchant', 'Date Created', '**USD', 'Attachments', 'Personal Note']
+    fields: ['Merchant', 'Transaction Date', '**USD', 'Attachments', 'Personal Note', 'Tags']
 });
 
-for (let manualAdd of manualAdds.records) {
-    let manualAddMerchant = manualAdd.getCellValue('Merchant');
-    let manualAddAmount = manualAdd.getCellValue('**USD');
-    let manualAddDate = manualAdd.getCellValue('Date Created');
-    let manualAddNote = manualAdd.getCellValue('Personal Note');
-    let manualAddAttachments = manualAdd.getCellValue('Attachments');
+// Check to make sure the record is still visibile in the 'Manual Add Check' view.
+// The record can appear in the view (triggering the automation) and then immediately dissapear if
+// the transaction quickly has a merchant added, and then is marked as approved in quick succession.
+if (transactionsFiltered.records.length > 0) {
+    console.log("Record is still visibile in 'Manual Add Check' view")
 
-    if (importMerchant.includes(manualAddMerchant[0].name) && (importAmount = manualAddAmount)) {
-        console.log('Match!');
+    let thisTransaction = transactionsFiltered.getRecord(importId);
 
-        if (thisTransaction.getCellValue('Tags')) {
-            let update = await transactions.updateRecordAsync(thisTransaction, {
-                'Transaction Date': manualAddDate,
-                'Personal Note': manualAddNote,
-                'Attachments': manualAddAttachments,
-                'Tags': [ ...thisTransaction.getCellValue('Tags'), { name: 'Manual Merged' }]
-            });
+    let manualAdds = await base.getTable('Transactions').getView('Manual Add').selectRecordsAsync({
+        fields: ['Merchant', 'Date Created', '**USD', 'Attachments', 'Personal Note']
+    });
+
+    for (let manualAdd of manualAdds.records) {
+        let manualAddMerchant = manualAdd.getCellValue('Merchant');
+        let manualAddAmount = manualAdd.getCellValue('**USD');
+        let manualAddDate = manualAdd.getCellValue('Date Created');
+        let manualAddNote = manualAdd.getCellValue('Personal Note');
+        let manualAddAttachments = manualAdd.getCellValue('Attachments');
+
+        if (importMerchant.includes(manualAddMerchant[0].name) && (importAmount == manualAddAmount)) {
+            console.log('Match!');
+
+            if (thisTransaction.getCellValue('Tags')) {
+                let update = await transactions.updateRecordAsync(thisTransaction, {
+                    'Transaction Date': manualAddDate,
+                    'Personal Note': manualAddNote,
+                    'Attachments': manualAddAttachments,
+                    'Tags': [ ...thisTransaction.getCellValue('Tags'), { name: 'Manual Merged' }]
+                });
+            } else {
+                let update = await transactions.updateRecordAsync(thisTransaction, {
+                    'Transaction Date': manualAddDate,
+                    'Personal Note': manualAddNote,
+                    'Attachments': manualAddAttachments,
+                    'Tags': [{ name: 'Manual Merged' }]
+                });
+            }
+
+            await transactions.deleteRecordAsync(manualAdd);
         } else {
-            let update = await transactions.updateRecordAsync(thisTransaction, {
-                'Transaction Date': manualAddDate,
-                'Personal Note': manualAddNote,
-                'Attachments': manualAddAttachments,
-                'Tags': [{ name: 'Manual Merged' }]
-            });
-        }
+            console.log('No match!');
 
-        await transactions.deleteRecordAsync(manualAdd);
-    } else {
-        console.log('No match!');
+            if (thisTransaction.getCellValue('Tags')) {
+                let update = await transactions.updateRecordAsync(thisTransaction, {
+                    'Tags': [ ...thisTransaction.getCellValue('Tags'), { name: 'No Manual Merge' }]
+                });
+            } else {
+                let update = await transactions.updateRecordAsync(thisTransaction, {
+                    'Tags': [{ name: 'No Manual Merge' }]
+                });
+            }
+        }
+    }
+
+    if (manualAdds.records.length == 0) {
+        console.log('No manually added records to match!');
 
         if (thisTransaction.getCellValue('Tags')) {
             let update = await transactions.updateRecordAsync(thisTransaction, {
@@ -55,18 +84,7 @@ for (let manualAdd of manualAdds.records) {
             });
         }
     }
+} else {
+    console.log("Record disappeared from 'Manual Add Check' view")
 }
 
-if (manualAdds.records.length == 0) {
-    console.log('No manually added records to match!');
-
-    if (thisTransaction.getCellValue('Tags')) {
-        let update = await transactions.updateRecordAsync(thisTransaction, {
-            'Tags': [ ...thisTransaction.getCellValue('Tags'), { name: 'No Manual Merge' }]
-        });
-    } else {
-        let update = await transactions.updateRecordAsync(thisTransaction, {
-            'Tags': [{ name: 'No Manual Merge' }]
-        });
-    }
-}
